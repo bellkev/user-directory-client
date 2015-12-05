20d309c246a9243c2ad86fa86178cb579503b65f
Kevin Bell <kevin.a.bell@gmail.com>
2015-12-05 15:27:07 -0800

    Couple tweaks

--- wait-for-stack.sh
+++ wait-for-stack.sh
@@ -5,6 +5,6 @@ set -exu
 curl --retry 10 --retry-delay 5 "http://$DOCKER_IP:8001"
 
 for i in {1..5}; do
-    psql -h $DOCKER_IP -U postgres -p 15432 <<<"\l" && break;
+    psql -h $DOCKER_IP -U postgres -p 15432 -c "\\l" && break;
     sleep 5;
 done
